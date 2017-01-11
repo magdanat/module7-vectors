576e3337e4af67e69322c7ed61f307b2485340cc
Nathan Magdalera <nathanmagdalera@gmail.com>
2017-01-10 18:25:05 -0800

    Add answers to exercise 1 and 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -21,7 +21,8 @@ len <- length(q)
 ### Bonus ###
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-
+even <- seq(2, 100, 2)
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
+test <- all(even %% 2 == 0)
